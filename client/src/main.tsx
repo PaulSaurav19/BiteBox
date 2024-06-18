1ad6958b1cd3cc3014ef884e21cb82ffb4aba6d4
ICONIC_07 <souravp9057@gmail.com>
2024-06-18 20:42:59 +0530

    created MyUserApi hook for creating user

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,15 +5,24 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+     <QueryClientProvider client = {queryClient}>
       <Auth0ProviderWithNavigate>
-
       <AppRoutes />
       </Auth0ProviderWithNavigate>
-      
+      </QueryClientProvider>
     </Router>
     <Toaster />
   </React.StrictMode>,
