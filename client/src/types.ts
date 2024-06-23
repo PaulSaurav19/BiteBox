daf3df2d7fc7e9ee6f8d6ac802775e685471a6f8
ICONIC_07 <souravp9057@gmail.com>
2024-06-23 12:14:21 +0530

    populated user profile form in frontend

---
+++ client/src/types.ts
@@ -0,0 +1,14 @@
+// we will define the properties and properties type based on the response we get
+
+export type User = {
+    _id: string;
+    email: string;
+    name: string;
+    contactNumber: string;
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    state: string;
+    country: string;
+    pincode: string;
+}
