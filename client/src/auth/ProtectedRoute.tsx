25a4e7377a899a5ac92121c3c44849d649772a85
ICONIC_07 <souravp9057@gmail.com>
2024-07-09 15:01:55 +0530

    fixed the reload bug

--- client/src/auth/ProtectedRoute.tsx
+++ client/src/auth/ProtectedRoute.tsx
@@ -2,14 +2,23 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const {isAuthenticated} = useAuth0();
+  const {isAuthenticated, isLoading} = useAuth0();
 
 // outlet means render all the child routes of this component
 // if the user is authenticated
 // its kind of like a children prop that we get whenever we have nested components
 // and if they are not logged in then return to the homepage
 // replace means it indicates to be a new URL
-  return isAuthenticated ? <Outlet /> : <Navigate to = "/" replace/>;
-}
+
+  if(isLoading) {
+    return null;
+  }
+
+  if(isAuthenticated) {
+    return <Outlet />
+  }
+
+  return <Navigate to="/" replace />
+};
 
 export default ProtectedRoute;
