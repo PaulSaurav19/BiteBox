fd3d2fd48264f762a08abb4cab49c3eff73b1ef0
ICONIC_07 <souravp9057@gmail.com>
2024-07-11 20:38:01 +0530

    updated the My Restaurant Api also

--- client/src/api/MyRestaurantApi.tsx
+++ client/src/api/MyRestaurantApi.tsx
@@ -62,8 +62,42 @@ export const useCreateMyRestaurant = () => {
   }
 
   if (error) {
-    toast.error("Unable to update restaurant");
+    toast.error("Unable to create restaurant");
   }
 
   return { createRestaurant, isLoading };
 };
+
+export const useUpdateMyRestaurant = () => {
+  const {getAccessTokenSilently} = useAuth0();
+
+  const updateRestaurantRequest = async(restaurantFormData: FormData): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+      body: restaurantFormData,
+    });
+
+    if(!response) {
+      throw new Error("Failed to update restaurant")
+    }
+
+    return response.json();
+  };
+
+  const {mutate: updateRestaurant, isLoading, error, isSuccess} = useMutation(updateRestaurantRequest);
+
+  if(isSuccess) {
+    toast.success("Restaurant updated")
+}
+
+  if(error) {
+    toast.error("Unable to update restaurant");
+  }
+
+  return {updateRestaurant, isLoading};
+}
