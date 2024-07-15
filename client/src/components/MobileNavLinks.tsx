ab43ec05bffe63aa6d7904234e2e7fbfd516700d
ICONIC_07 <souravp9057@gmail.com>
2024-07-15 12:51:35 +0530

    Updated the restaurant api and moblile nav links to include my Restaurant Page

--- client/src/components/MobileNavLinks.tsx
+++ client/src/components/MobileNavLinks.tsx
@@ -6,6 +6,10 @@ const MobileNavLinks = () => {
     const {logout} = useAuth0();
   return (
     <>
+         <Link
+        to="/manage-restaurant" className="flex bg-white items-center font-bold hover:text-orange-500">
+        My Restaurant
+      </Link>
         <Link to="/user-profile" className="flex bg-white items-center font-bold hover:text-customOrange-500">
             User Profile
         </Link>
