c41c3817b1b678dbeee2cb9c7edb17bfad5565f9
ICONIC_07 <souravp9057@gmail.com>
2024-07-15 11:58:31 +0530

    updated the search Result card

--- client/src/components/SearchResultCard.tsx
+++ client/src/components/SearchResultCard.tsx
@@ -33,7 +33,7 @@ const SearchResultCard = ({restaurant}: Props) => {
             </div>
             <div className="flex items-center gap-1">
                 <Banknote/>
-                Delivery from ₹{(restaurant.deliveryPrice / 100).toFixed(2)}
+                Delivery Price ₹{(restaurant.deliveryPrice / 100).toFixed(2)}
             </div>
         </div>
         </div>
