50444383f93c6dbec0396e8c80225fc761a7f779
ICONIC_07 <souravp9057@gmail.com>
2024-07-16 13:33:30 +0530

    Added the get Restaurant endpoint in backend

--- server/src/routes/RestaurantRoute.ts
+++ server/src/routes/RestaurantRoute.ts
@@ -5,6 +5,10 @@ import RestaurantController from "../controllers/RestaurantController";
 const router = express.Router();
 
 // api/restaurant/search/prayagraj
+
+router.get("/:restaurantId", param("restaurantId").isString().trim().notEmpty().withMessage("RestaurantId Parameter must be a valid string"),
+RestaurantController.getRestaurant);
+
 router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City Parameter must be a valid string"),
 RestaurantController.searchRestaurant
 );
