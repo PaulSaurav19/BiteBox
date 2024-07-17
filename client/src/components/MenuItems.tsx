31106da956c087a516b61fcd6f7dacb13aeb0daf
ICONIC_07 <souravp9057@gmail.com>
2024-07-17 15:40:44 +0530

    Added the Menu Item section to be displayed

--- client/src/components/MenuItems.tsx
+++ client/src/components/MenuItems.tsx
@@ -4,24 +4,36 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 type Props = {
   menuItem: MenuItem;
+  addToCart: (menuItem: MenuItem,  action: 'increment' | 'decrement') => void;
 };
 
-const MenuItems = ({ menuItem }: Props) => {
+const MenuItems = ({ menuItem, addToCart }: Props) => {
   const [quantity, setQuantity] = useState(0);
 
-  const increment = () => setQuantity(quantity + 1);
-  const decrement = () => setQuantity(quantity > 0 ? quantity - 1 : 0);
+  const increment = () => { 
+    setQuantity(quantity + 1);
+    addToCart(menuItem, 'increment');
+  };
+
+  const decrement = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+      addToCart(menuItem, 'decrement');
+    }
+  };
 
   return (
-    <Card className="cursor-pointer p-4 shadow-md rounded-lg">
+    <Card className="cursor-pointer p-6 shadow-md rounded-lg transition-transform transform hover:scale-105">
       <CardHeader className="mb-2">
-        <CardTitle className="text-xl font-semibold">{menuItem.name}</CardTitle>
+        <CardTitle className="text-2xl font-semibold">• {menuItem.name}</CardTitle>
       </CardHeader>
       <CardContent className="font-bold">
+      <p className="text-lg font-semibold text-gray-700 mb-4">
         ₹{(menuItem.price / 100).toFixed(2)}
-        <div className="flex items-center  justify-center mt-4 space-x-4">
+        </p>
+        <div className="flex items-center justify-center mt-4 space-x-4">
         <button
-            className="bg-red-500 text-white w-10 h-10 flex items-center justify-center rounded-full border border-red-700"
+            className="bg-red-500 text-white w-10 h-10 flex items-center justify-center rounded-full border border-red-700 transition duration-300 hover:bg-red-600"
           onClick={decrement}
           disabled={quantity === 0}
         >
@@ -29,7 +41,7 @@ const MenuItems = ({ menuItem }: Props) => {
         </button>
         <span className="text-xl font-semibold">{quantity}</span>
         <button
-           className="bg-green-500 text-white w-10 h-10 flex items-center justify-center rounded-full border border-green-700"
+           className="bg-green-500 text-white w-10 h-10 flex items-center justify-center rounded-full border border-green-700 transition duration-300 hover:bg-green-600"
           onClick={increment}
         >
           +
