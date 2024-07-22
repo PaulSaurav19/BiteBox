89108c4a067c2ab4b6afbca019b021561f75863e
ICONIC_07 <souravp9057@gmail.com>
2024-07-22 22:46:45 +0530

    Added the order status component

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 import UsernameMenu from "./UsernameMenu";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const MainNav = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -26,7 +27,14 @@ const MainNav = () => {
   return(
     <div>
     <span className="flex space-x-2 items-center">
-      {isAuthenticated ? ( <UsernameMenu /> ) : ( <Button className="bg-customBlue-500 text-white font-bold py-2 px-4 rounded-lg shadow-lg hover:text-white hover:bg-customBlue-100 hover:border-customBlue-500 hover:border transition-all duration-300 ease-in-out transform hover:scale-105"
+      {isAuthenticated ? (
+        <>
+          <Link to="/order-status" className="font-bold hover:text-customOrange-500">
+             Order Status 
+          </Link>
+           <UsernameMenu /> 
+        </>
+        ) : ( <Button className="bg-customBlue-500 text-white font-bold py-2 px-4 rounded-lg shadow-lg hover:text-white hover:bg-customBlue-100 hover:border-customBlue-500 hover:border transition-all duration-300 ease-in-out transform hover:scale-105"
       onClick={async() => await loginWithRedirect()}
     >
         Log In
