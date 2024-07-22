fe47cf42e3d57f45f557ed7abfd237228a93d29b
ICONIC_07 <souravp9057@gmail.com>
2024-07-22 22:46:59 +0530

    Updated the mobile nav links

--- client/src/components/MobileNavLinks.tsx
+++ client/src/components/MobileNavLinks.tsx
@@ -6,6 +6,10 @@ const MobileNavLinks = () => {
     const {logout} = useAuth0();
   return (
     <>
+      <Link to="/order-status" className="flex bg-white items-center font-bold hover:text-customOrange-500">
+            Order Status
+      </Link>
+
          <Link
         to="/manage-restaurant" className="flex bg-white items-center font-bold hover:text-orange-500">
         My Restaurant
