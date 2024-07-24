7a38a499076ff1fe95c30d6968ac5d666fa29fd9
ICONIC_07 <souravp9057@gmail.com>
2024-07-24 10:38:46 +0530

    Added the hook in the frontend api for updating order status

--- client/src/api/MyRestaurantApi.tsx
+++ client/src/api/MyRestaurantApi.tsx
@@ -130,5 +130,56 @@ export const useGetMyRestaurantOrders = () => {
   return { orders, isLoading };
 };
 
+type UpdateOrderStatusRequest = {
+  orderId: string;
+  status: string;
+};
+
+export const useUpdateMyRestaurantOrder = () => {
+  const {getAccessTokenSilently} = useAuth0();
+
+  const updateMyRestaurantOrder = async(updateStatusOrderRequest: UpdateOrderStatusRequest) => {
+    
+      const accessToken = await getAccessTokenSilently();
+
+      const response = await fetch(
+        `${API_BASE_URL}/api/my/restaurant/order/${updateStatusOrderRequest.orderId}/status`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ status: updateStatusOrderRequest.status }),
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to update status");
+      }
+  
+      return response.json();
+    };
+  
+    const {
+      mutateAsync: updateRestaurantStatus,
+      isLoading,
+      isError,
+      isSuccess,
+      reset,
+    } = useMutation(updateMyRestaurantOrder);
+  
+    if (isSuccess) {
+      toast.success("Order updated");
+    }
+  
+    if (isError) {
+      toast.error("Unable to update order");
+      reset(); // so that the error state doesnt gets popped all the time 
+    }
+  
+    return { updateRestaurantStatus, isLoading };
+  };
+
 
 
